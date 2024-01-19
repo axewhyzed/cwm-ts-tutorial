eff22f32772ec6d0caef0c9b5dc129d96775d7bf
axewhyzed <coding0234@gmail.com>
2024-01-19 16:29:56 +0530

    best practices

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,7 +1,7 @@
-// const small = 1;
-// const medium = 2;
-// const large = 3;
+function calculateTax(income: number, taxYear = 2022):number{
+    if(taxYear < 2022)
+        return income*1.2;
+    return income*1.3;
+}
 
-const enum Size { Small = 1, Medium, Large };
-let mySize:Size = Size.Medium;
-console.log(mySize);
+calculateTax(10_000, 2023);
