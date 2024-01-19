ba4ab987b2decba99d6a02abef5b34a4514bc038
axewhyzed <coding0234@gmail.com>
2024-01-19 16:55:07 +0530

    literal types

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,14 +1,20 @@
-type Draggable = {
-    drag: () => void
-};
+// type Draggable = {
+//     drag: () => void
+// };
 
-type Resizable = {
-    resize: () => void;
-};
+// type Resizable = {
+//     resize: () => void;
+// };
 
-type UIWidget = Draggable & Resizable;
+// type UIWidget = Draggable & Resizable;
 
-let textBox:UIWidget = {
-    drag: () => {},
-    resize: () => {},
-};
+// let textBox:UIWidget = {
+//     drag: () => {},
+//     resize: () => {},
+// };
+
+// Literal ( exact , specific )
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
+
+type Metric = 'cm' | 'inch';
