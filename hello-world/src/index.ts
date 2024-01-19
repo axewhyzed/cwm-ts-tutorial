8b2d86bc8159ccda03d9abc89b38e7bb58d7a9d3
axewhyzed <coding0234@gmail.com>
2024-01-19 15:46:38 +0530

    Any data type

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,6 +1,4 @@
-let age:number = 20
-if(age<50)
-    age += 10;
-console.log(age);
-
-// Then type tsc index.ts
+let sales = 123_456_789; // _ added to make code more readable, it is still 123456789
+let course = 'Typescript';
+let is_published = true;
+let level;
