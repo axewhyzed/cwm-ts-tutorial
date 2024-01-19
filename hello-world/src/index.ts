0420758f482ba69d5995671cf5192582ade6ea1b
axewhyzed <coding0234@gmail.com>
2024-01-19 16:15:07 +0530

    enums

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,2 +1,7 @@
-let user: [number, string, boolean, number] = [1, 'Mihir', true, 0];
-// Tuple
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+const enum Size { Small = 1, Medium, Large };
+let mySize:Size = Size.Medium;
+console.log(mySize);
