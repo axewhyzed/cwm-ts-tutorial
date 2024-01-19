4c8aa106c04cd4e3a9598bae08ad50e1f90c89b5
axewhyzed <coding0234@gmail.com>
2024-01-19 15:52:50 +0530

    arrays

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,4 +1,2 @@
-let sales = 123_456_789; // _ added to make code more readable, it is still 123456789
-let course = 'Typescript';
-let is_published = true;
-let level;
+let numbers:number[] = [];
+numbers.forEach(n=> n.toFixed);
